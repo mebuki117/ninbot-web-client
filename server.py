f42b902419b37a5244ee367e5cb6343a4e64de95
mebuki117 <97399080+mebuki117@users.noreply.github.com>
2024-10-09 22:09:50 +0900

    Improvement of readability

--- server.py
+++ server.py
@@ -24,19 +24,23 @@ class DataFetcher:
         self.data = {}
         self.error = None
         
-        # sorry
-        self.t1 = threading.Thread(target=lambda: self._sse_worker(STRONGHOLD_SSE_URL, "stronghold"), daemon=True)
-        self.t2 = threading.Thread(target=lambda: self._sse_worker(BOAT_SSE_URL, "boat"), daemon=True)
-        self.t3 = threading.Thread(target=lambda: self._sse_worker(BLIND_SSE_URL, "blind"), daemon=True)
-        self.t4 = threading.Thread(target=lambda: self._sse_worker(DIVINE_SSE_URL, "divine"), daemon=True)
-        self.t5 = threading.Thread(target=self.fetch_version, daemon=True)
+        self.sse_urls = [
+            (STRONGHOLD_SSE_URL, "stronghold"),
+            (BOAT_SSE_URL, "boat"),
+            (BLIND_SSE_URL, "blind"),
+            (DIVINE_SSE_URL, "divine")
+        ]
+        
+        self.threads = [
+            threading.Thread(target=lambda url=url, key=key: self._sse_worker(url, key), daemon=True)
+            for url, key in self.sse_urls
+        ]
+        
+        self.threads.append(threading.Thread(target=self.fetch_version, daemon=True))
 
     def start(self):
-        self.t1.start()
-        self.t2.start()
-        self.t3.start()
-        self.t4.start()
-        self.t5.start()
+        for thread in self.threads:
+            thread.start()
 
     def _sse_worker(self, url, data_name):
         while True:
@@ -59,7 +63,6 @@ class DataFetcher:
     
     def get_data(self):
         return self.data
-
 
     # def _sse_worker(self, url, data_name):
     #     while True:
